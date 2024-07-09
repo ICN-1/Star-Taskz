fc3d3a5d81dc828147572be71f2abe38accfe8fe
Ifechukwu Nwachukwu <ifechukwucnwachukwu@gmail.com>
2024-07-09 13:10:50 +0100

    Profile Page UI done

--- app/src/main/java/com/theteam/taskz/ViewProfileActivity.java
+++ app/src/main/java/com/theteam/taskz/ViewProfileActivity.java
@@ -1,14 +1,24 @@
 package com.theteam.taskz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.theteam.taskz.adapters.ViewPagerAdapter;
 
 public class ViewProfileActivity extends AppCompatActivity {
+    ImageView goToSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,14 @@ public class ViewProfileActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        goToSettings = findViewById(R.id.goToSettings);
+        goToSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ViewProfileActivity.this, SettingsActivity.class));
+            }
         });
     }
 }
